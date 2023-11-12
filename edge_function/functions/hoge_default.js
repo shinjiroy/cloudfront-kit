e974f3268501f02926d546f3e48bda5fb94dc2ea
shinjiroy <shinjiroyamaguchi829@gmail.com>
2023-11-12 22:50:39 +0900

    まめちしき

--- edge_function/functions/hoge_default.js
+++ edge_function/functions/hoge_default.js
@@ -3,5 +3,8 @@ function handler(event) {
 
     // 何か処理があれば
 
+    // 豆知識
+    // requestの中身(cookieやheader)が多いとそれだけreturn requestするタイミングでコンピューティング使用率が多くなります。
+    // あんまり多すぎて遅い場合は、フィルタリングするなりすると改善が期待できます。
     return request;
 }
